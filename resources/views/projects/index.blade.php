ebe7e4cc11a0cac1fc1365f4d20ca0524ece70ad
hendrikus <hendrikus.richardo@cyberprimatama.co.id>
2019-03-21 17:20:31 +0700

    second commit

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Birdboard</h1>
+
+    <ul>
+        @foreach ($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
+    
+</body>
+</html>
